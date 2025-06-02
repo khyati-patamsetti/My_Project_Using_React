85c90aa19b650ba9ed8840505fac4da43a5b4243
khyati-patamsetti <khyatipatamsetti09@gmail.com>
2025-06-02 15:12:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,88 @@
-import './App.css';
-import axios from 'axios';
-import Contact from './components/Contact/contact';
-import Footer1 from './components/footer/footer1';
-import HomePage from './components/homepage/HomePage';
-import PeriodCalendar from './components/PeriodCalendar/calendar';
-import Services from './components/servicespage/ServicesPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RegistrationForm from "./components/LoginSignin/registration";
-import GenderSelection from "./components/Fitness/fit";
-import GoalSelection from "./components/Fitness/GoalSelection";
-import FocusAreaSelection from "./components/Fitness/focus";
-import HeightSelection from "./components/Fitness/heightselection";
-import WeightSelection from "./components/Fitness/weight";
-import Dashboard from './components/Dashboard/dashboard'; 
-import Navigate from './components/navigate/navigate'
-import Diet from './components/Nutritious/cardtask'
-import Exercise from './components/exercise/Exercise'
-import Aerobic from './components/exercise/aerobic'
-import Anaerobic from './components/exercise/anaerobic'
-import Balance from './components/exercise/balance'
-import Flexibility from './components/exercise/flexibility'
-import Exe from './components/exercise/exc'
-import Strength from './components/exercise/strength'
-import Yoga from './components/yoga/classcompo'
-import Air from './components/yoga/air'
-import Ashtanga from './components/yoga/Ashtanga'
-import Hatha from './components/yoga/hatha'
-import Lyengar from './components/yoga/lyengar'
-import Bikram from './components/yoga/bikram'
-import Facial from './components/yoga/facial'
-import Mantra from "./components/meditation/mantra";
-import MeditationSelectionPage from "./components/meditation/MeditationDetailsPage";
-import Chakra from "./components/meditation/chakra";
-import Movement from "./components/meditation/movement";
-import Spiritual from "./components/meditation/spiritual";
-import Visualization from "./components/meditation/visualization";
-import Meditation from "./components/meditation/Meditation";
+// import './App.css';
+// import axios from 'axios';
+// import Contact from './components/Contact/contact';
+// import Footer1 from './components/footer/footer1';
+// import HomePage from './components/homepage/HomePage';
+// import PeriodCalendar from './components/PeriodCalendar/calendar';
+// import Services from './components/servicespage/ServicesPage';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import RegistrationForm from "./components/LoginSignin/registration";
+// import GenderSelection from "./components/Fitness/fit";
+// import GoalSelection from "./components/Fitness/GoalSelection";
+// import FocusAreaSelection from "./components/Fitness/focus";
+// import HeightSelection from "./components/Fitness/heightselection";
+// import WeightSelection from "./components/Fitness/weight";
+// import Dashboard from './components/Dashboard/dashboard'; 
+// import Navigate from './components/navigate/navigate'
+// import Diet from './components/Nutritious/cardtask'
+// import Exercise from './components/exercise/Exercise'
+// import Aerobic from './components/exercise/aerobic'
+// import Anaerobic from './components/exercise/anaerobic'
+// import Balance from './components/exercise/balance'
+// import Flexibility from './components/exercise/flexibility'
+// import Exe from './components/exercise/exc'
+// import Strength from './components/exercise/strength'
+// import Yoga from './components/yoga/classcompo'
+// import Air from './components/yoga/air'
+// import Ashtanga from './components/yoga/Ashtanga'
+// import Hatha from './components/yoga/hatha'
+// import Lyengar from './components/yoga/lyengar'
+// import Bikram from './components/yoga/bikram'
+// import Facial from './components/yoga/facial'
+// import Mantra from "./components/meditation/mantra";
+// import MeditationSelectionPage from "./components/meditation/MeditationDetailsPage";
+// import Chakra from "./components/meditation/chakra";
+// import Movement from "./components/meditation/movement";
+// import Spiritual from "./components/meditation/spiritual";
+// import Visualization from "./components/meditation/visualization";
+// import Meditation from "./components/meditation/Meditation";
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<HomePage />} />
+//         <Route path='/login' element={<RegistrationForm />} />
+//         <Route path="/gender" element={<GenderSelection />} />
+//         <Route path="/goal-selection" element={<GoalSelection />} />
+//         <Route path="/focus" element={<FocusAreaSelection />} />
+//         <Route path="/height" element={<HeightSelection />} />
+//         <Route path="/weight" element={<WeightSelection />} />
+//         <Route path="/dashboard" element={<Dashboard   />} /> 
+//         <Route path="/navigate" element={<Navigate />} />
+//         <Route path="/aerobic" element={<Aerobic />} />
+//         <Route path="/anaerobic" element={<Anaerobic />} />
+//         <Route path="/balance" element={<Balance />} />
+//         <Route path="/flexibility" element={<Flexibility />} />
+//         <Route path="/exe" element={<Exe />} />
+//         <Route path="/strength" element={<Strength />} />
+//         <Route path="/exercise" element={<Exercise />} />
+//         <Route path="/diet" element={<Diet />} />
+//         <Route path="/yoga" element={<Yoga />} />
+//       <Route path="/air-yoga" element={<Air/>}></Route>
+//       <Route path="/ashtanga-yoga" element={<Ashtanga/>}></Route>
+//       <Route path="/hatha-yoga" element={<Hatha/>}></Route>
+//       <Route path="/iyengar-yoga" element={<Lyengar/>}></Route>
+//       <Route path="/bikram-yoga"   element={<Bikram/>}></Route>
+//       <Route path="/facial-yoga" element={<Facial/>}></Route>
+//       <Route path="/meditation" exact Component={Meditation}></Route>
+//     <Route path="/mantra" element={<Mantra />} />
+//         <Route path="/mindful" element={<MeditationSelectionPage />} />
+//         <Route path="/chakra" element={<Chakra />} />
+//         <Route path="/movement" element={<Movement/>} />
+//         <Route path="/visualization" element={<Visualization/>} />
+//         <Route path="/spiritual" element={<Spiritual />} />
+//         <Route path="/period-calendar" element={<PeriodCalendar />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<RegistrationForm />} />
-        <Route path="/gender" element={<GenderSelection />} />
-        <Route path="/goal-selection" element={<GoalSelection />} />
-        <Route path="/focus" element={<FocusAreaSelection />} />
-        <Route path="/height" element={<HeightSelection />} />
-        <Route path="/weight" element={<WeightSelection />} />
-        <Route path="/dashboard" element={<Dashboard   />} /> 
-        <Route path="/navigate" element={<Navigate />} />
-        <Route path="/aerobic" element={<Aerobic />} />
-        <Route path="/anaerobic" element={<Anaerobic />} />
-        <Route path="/balance" element={<Balance />} />
-        <Route path="/flexibility" element={<Flexibility />} />
-        <Route path="/exe" element={<Exe />} />
-        <Route path="/strength" element={<Strength />} />
-        <Route path="/exercise" element={<Exercise />} />
-        <Route path="/diet" element={<Diet />} />
-        <Route path="/yoga" element={<Yoga />} />
-      <Route path="/air-yoga" element={<Air/>}></Route>
-      <Route path="/ashtanga-yoga" element={<Ashtanga/>}></Route>
-      <Route path="/hatha-yoga" element={<Hatha/>}></Route>
-      <Route path="/iyengar-yoga" element={<Lyengar/>}></Route>
-      <Route path="/bikram-yoga"   element={<Bikram/>}></Route>
-      <Route path="/facial-yoga" element={<Facial/>}></Route>
-      <Route path="/meditation" exact Component={Meditation}></Route>
-    <Route path="/mantra" element={<Mantra />} />
-        <Route path="/mindful" element={<MeditationSelectionPage />} />
-        <Route path="/chakra" element={<Chakra />} />
-        <Route path="/movement" element={<Movement/>} />
-        <Route path="/visualization" element={<Visualization/>} />
-        <Route path="/spiritual" element={<Spiritual />} />
-        <Route path="/period-calendar" element={<PeriodCalendar />} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{ color: 'white', backgroundColor: 'black', height: '100vh' }}>
+      <h1>Hello GitHub Pages</h1>
+    </div>
   );
 }
 
-export default App;
